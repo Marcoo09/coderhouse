d58bd0126d22ec8931b896d7315345634e5910f2
Marcoo09 <marcofiorito1@hotmail.com>
2016-06-08 21:20:24 -0300

    Update clase3.js

--- TP2/clase3.js
+++ TP2/clase3.js
@@ -3,6 +3,11 @@
 var  suma=0;
 
 function esPrimo(a){
+	if(a===1 || a===2){
+		
+		return "Es Primo";
+	}else{
+	
 	for(i=1;i<a-1;i++){
 		if(a%i===0){
 			
@@ -14,6 +19,7 @@ function esPrimo(a){
 		return "Es primo";
 	}else{
 		return "No es primo";
+	}
 	}
 	
 }
