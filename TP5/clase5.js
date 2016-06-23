1b9c9134ac27ef55b0262df7048c7a8fb0a0ce8a
Marcoo09 <marcofiorito1@hotmail.com>
2016-06-23 14:54:20 -0300

    Update clase5.js

--- TP5/clase5.js
+++ TP5/clase5.js
@@ -44,3 +44,37 @@ function mySort() {
 }
 
 mySort(1,2,4,5,6,1,8,9,4,8,0)
+
+
+//MAS OPTIMIZADO
+
+function ordenar(){
+var lugar;
+var limite=arguments.length;
+var matriz= [];
+//EL BUCLE FOR QUE OPERA CONSTANTEMENTE ES EL DE LA VARIABLE I POR ESO ES QUE COMPARO SI MATRS[J] ESMAYOR QUE MATRIZ [I]
+//PORQUE MATRIZ[I] ESTA EN UN LUGAR MAS GRANDE EN EL ARRAY Y SI EL IF ES TRUE SE CAMBIAN DE LUGAR
+for(var i=0;i<limite;i++){
+	matriz.push(arguments[i]);
+	
+	for(var j=0;j<i;j++){
+		if(matriz[j]>matriz[i]){
+			lugar= matriz[j];
+			matriz[j]=matriz[i];
+			matriz[i]=lugar;
+			
+			
+		}
+	
+		
+	
+	}
+	
+		
+}
+return matriz;
+
+}
+
+
+ordenar(2,5,7,1,5,3);
